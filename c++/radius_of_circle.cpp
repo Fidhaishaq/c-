028f0bb5270dc60c550f493567d9b3076a37d9dd
Fidhaishaq <168504737+Fidhaishaq@users.noreply.github.com>
2024-06-07 19:42:48 +0530

    Add files via upload

---
+++ c++/radius_of_circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int radius=2;
+    const float PI=3.14;
+    float area=PI*radius*radius;
+    cout<<"area of the circle with the radius "<<radius<< " is: "<<area;
+return 0;
+}
---
+++ c++/sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{int num1,num2,result;
+cout << "Enter a number: "<<endl;
+cin >> num1;
+cout <<"Enter the second number: "<<endl;
+cin >> num2;
+result=num1+num2;
+cout<<"The sum of these two numbers "<<num1 <<" and "<<num2 <<" are "<<result<<endl;
+return 0;
+}
